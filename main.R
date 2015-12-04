55e57c68c5e6f1113f03b4ae0fd16c7b4ce7682b
pcosta74 <pedro.mpm.costa@gmail.com>
2015-12-04 12:33:10 +0000

    Update main.R

--- main.R
+++ main.R
@@ -2,8 +2,8 @@ source('io.R')
 source('naivebayes.R')
 
 run<-function(formula, train.csv, test.csv, out.csv) {
-  train.data<-read.data.frame(train.csv)
-  test.data<-read.data.frame(test.csv)
+  train.data<-read.data.frame(train.csv, blank.strings=c("","?"))
+  test.data<-read.data.frame(test.csv, blank.strings=c("","?"))
   pred.data<-naivebayes(formula, train.data, test.data)
   write.data.frame(pred.data,out.csv)
 }
