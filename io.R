e42028cceede8ebe8e307f12131164ec28df1612
pcosta74 <pedro.mpm.costa@gmail.com>
2015-12-04 12:25:49 +0000

    Update io.R

--- io.R
+++ io.R
@@ -3,8 +3,11 @@ source('common.R')
 # Read a CVS file into a data frame
 # path - path to the CVS file
 # handle.blanks - how to handle lines with blank cells { 0-raise error, 1-skip lines, 2-use most commons values in column }
-read.data.frame <- function(path, handle.blanks = 2) {
-  dataframe <- read.csv(path,header = TRUE, na.strings = c("",NA), strip.white = TRUE)
+read.data.frame <- function(path, blank.strings = NULL) {
+  if(is.character(blank.strings) | is.vector(blank.strings))
+    blank.strings<-unique(c(blank.strings,NA))
+  
+  dataframe <- read.csv(path,header = TRUE, na.strings = blank.strings, strip.white = TRUE)
   attr(dataframe, "relation")<-substr(path,1,regexpr("\\.",path) - 1)
   
   if (is.empty.data.frame(dataframe))
@@ -18,26 +21,23 @@ read.data.frame <- function(path, handle.blanks = 2) {
   if(length(cells.NA)>0) {
     msg<-paste(apply(cells.NA,1,function(r) paste(LETTERS[r["col"]],r["row"],sep="")), collapse = "; ")
     
-    switch(as.character(handle.blanks),
-       "1" = {
-          warning("\tEmpty cell(s) found: ", msg, "\n\tIgnoring lines", immediate. = TRUE)
-          dataframe <- na.omit(dataframe)
-        }, 
-        "2" = {
-          warning("\tEmpty cell(s) found: ", msg, "\n\tUsing most common values in columns" ,immediate. = TRUE)
-          common.vals<-sapply(dataframe, function(x) {
-            s<-summary(x[!is.na(x)])
-            return(names(which.max(s[!is.na(names(s))])))
-          })
-          for(col in colnames(dataframe))
-            dataframe[which(is.na(dataframe[,col])),col]<-common.vals[col]
-        }, 
-        {
-          stop("\tEmpty cell(s) found: ", msg, "\n\tAborting!")
-        }
-    )
+    if(is.null(blank.strings)) {
+       warning("\tEmpty cell(s) found: ", msg, "\n\tIgnoring lines", immediate. = TRUE)
+       dataframe <- na.omit(dataframe)
+    } else {
+      warning("\tEmpty cell(s) found: ", msg, "\n\tUsing most common values in columns" ,immediate. = TRUE)
+      common.vals<-sapply(dataframe, function(x) {
+        s<-summary(x[!is.na(x)])
+        return(names(which.max(s[!is.na(names(s))])))
+        })
+      for(col in colnames(dataframe))
+        dataframe[which(is.na(dataframe[,col])),col]<-common.vals[col]
+    } 
   }
   
+  message("read ",nrow(dataframe), " rows from '",path,"'")
+  return(dataframe)
+}  
   message("read ",nrow(dataframe), " rows from '",path,"'")
   return(dataframe)
 }
