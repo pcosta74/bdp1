3cff35875acd33cb4180fdb150fa160bf17c721d
Pedro Costa <pedro.mpm.costa@gmail.com>
2015-12-22 22:12:41 +0000

    added plot output for files

--- test.R
+++ test.R
@@ -6,59 +6,72 @@ scenario<-list(
     formula=eval ~ .,
     train="datasets/cars_train.csv", 
     pred="datasets/cars_pred.csv",
-    output="output/cars_pred.csv"
+    output="output/cars_pred.csv",
+    plot="output/cars_pred.png"
   ),
   'tennis'=list(
     formula=play ~ .,
     train="datasets/tennis_train.csv", 
     pred="datasets/tennis_pred.csv",
     output="output/tennis_pred.csv",
+    plot="output/tennis_pred.png",
     percent.split=1
   ),
   'clients'=list(
     formula=emprestimo ~ .,
     train="datasets/clientes_train.csv", 
     pred="datasets/clientes_pred.csv",
-    output="output/clientes_pred.csv"
+    output="output/clientes_pred.csv",
+    plot="output/clientes_pred.png"
   ),
   'scale'=list(
     formula=scales ~ .,
     train="datasets/balance_scale_train.csv", 
     pred="datasets/balance_scale_pred.csv",
-    output="output/balance_scale_pred.csv"
+    output="output/balance_scale_pred.csv",
+    plot="output/balance_scale_pred.png"
   ),
   'votes'=list(
     formula=party ~ .,
     train="datasets/house_votes_84_train.csv", 
     pred="datasets/house_votes_84_pred.csv",
-    output="output/house_votes_84_pred.csv"
+    output="output/house_votes_84_pred.csv",
+    plot="output/house_votes_84_pred.png"
   ),
   'lenses'=list(
     formula=contact_lenses ~ .,
     train="datasets/lenses_train.csv", 
     pred="datasets/lenses_pred.csv",
     output="output/lenses_pred.csv",
+    plot="output/lenses_pred.png",
     percent.split=1
   ),
   'transfusion'=list(
     formula=donated ~ .,
     train="datasets/transfusion_train.csv", 
     pred="datasets/transfusion_pred.csv",
-    output="output/transfusion_pred.csv"
+    output="output/transfusion_pred.csv",
+    plot="output/transfusion_pred.png"
   ) #,
   #'poker'=list(
   #  formula=Poker_Hand ~ .,
   #  train="datasets/poker_hand_train.csv", 
   #  pred="datasets/poker_hand_pred.csv",
-  #  output="output/poker_hand_pred.csv"
+  #  output="output/poker_hand_pred.csv",
+  #  plot="output/poker_hand_pred.png"
   #)
 )
 
-test<-function(formula,train, pred=NULL, output=NULL, percent.split=0.7) {
+test<-function(formula,train, pred=NULL, output=NULL, plot=NULL, percent.split=0.7) {
   train.data<-read.data.frame(train, na.strings=c("","?"))
   pred.data<-read.data.frame(pred, na.strings=c("","?"))
   out.data<-naivebayes(formula, train.data, pred.data, percent.split)
   write.data.frame(out.data, output)
+  if(!is.null(plot)) {
+    w<-600; h<-600
+    dev.copy(png, plot, width=w, height=h)
+    dev.off()
+  }
 }
 
 run<-function() {
