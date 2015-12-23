15a87814472cb6e5e2eac7dff64f838cd67d58a1
Pedro Costa <pedro.mpm.costa@gmail.com>
2015-12-23 19:21:38 +0000

    several tests running

--- test.R
+++ test.R
@@ -6,73 +6,71 @@ scenario<-list(
     formula=eval ~ .,
     train="datasets/cars_train.csv", 
     pred="datasets/cars_pred.csv",
-    output="output/cars_pred.csv",
-    plot="output/cars_pred.png"
+    output="pred/cars_pred.csv",
+    plot="plot/cars_test.png"
   ),
   'tennis'=list(
     formula=play ~ .,
     train="datasets/tennis_train.csv", 
     pred="datasets/tennis_pred.csv",
-    output="output/tennis_pred.csv",
-    plot="output/tennis_pred.png",
-    percent.split=1
+    output="pred/tennis_pred.csv",
+    plot="plot/tennis_test.png"
   ),
   'clients'=list(
     formula=emprestimo ~ .,
     train="datasets/clientes_train.csv", 
     pred="datasets/clientes_pred.csv",
-    output="output/clientes_pred.csv",
-    plot="output/clientes_pred.png"
+    output="pred/clientes_pred.csv",
+    plot="plot/clientes_test.png"
   ),
   'scale'=list(
     formula=scales ~ .,
     train="datasets/balance_scale_train.csv", 
     pred="datasets/balance_scale_pred.csv",
-    output="output/balance_scale_pred.csv",
-    plot="output/balance_scale_pred.png"
+    output="pred/balance_scale_pred.csv",
+    plot="plot/balance_scale_test.png"
   ),
   'votes'=list(
     formula=party ~ .,
     train="datasets/house_votes_84_train.csv", 
     pred="datasets/house_votes_84_pred.csv",
-    output="output/house_votes_84_pred.csv",
-    plot="output/house_votes_84_pred.png"
+    output="pred/house_votes_84_pred.csv",
+    plot="plot/house_votes_84_test.png"
   ),
   'lenses'=list(
     formula=contact_lenses ~ .,
     train="datasets/lenses_train.csv", 
     pred="datasets/lenses_pred.csv",
-    output="output/lenses_pred.csv",
-    plot="output/lenses_pred.png",
-    percent.split=1
+    output="pred/lenses_pred.csv",
+    plot="plot/lenses_test.png"
   ),
   'transfusion'=list(
     formula=donated ~ .,
     train="datasets/transfusion_train.csv", 
     pred="datasets/transfusion_pred.csv",
-    output="output/transfusion_pred.csv",
-    plot="output/transfusion_pred.png"
+    output="pred/transfusion_pred.csv",
+    plot="plot/transfusion_test.png"
   ),
   'poker'=list(
     formula=Poker_Hand ~ .,
     train="datasets/poker_hand_train.csv", 
     pred="datasets/poker_hand_pred.csv",
-    output="output/poker_hand_pred.csv",
-    plot="output/poker_hand_pred.png"
+    output="pred/poker_hand_pred.csv",
+    plot="plot/poker_hand_test.png"
   ),
   'iris'=list(
     formula=Species ~ .,
     train="datasets/iris_train.csv", 
     pred="datasets/iris_pred.csv",
-    output="output/iris_pred.csv",
-    plot="output/iris_pred.png"
+    output="pred/iris_pred.csv",
+    plot="plot/iris_test.png"
   ),
   'titanic'=list(
-    formula=Class ~ .,
+    formula=Survived ~ .,
     train="datasets/titanic_train.csv", 
     pred="datasets/titanic_pred.csv",
-    output="output/titanic_pred.csv",
-    plot="output/titanic_pred.png"
+    output="pred/titanic_pred.csv",
+    plot="plot/titanic_test.png"
   )
 )
 
@@ -81,20 +79,23 @@ test<-function(formula,train, pred=NULL, output=NULL, plot=NULL, percent.split=0
   pred.data<-read.data.frame(pred, na.strings=c("","?"))
   out.data<-naivebayes(formula, train.data, pred.data, percent.split)
   write.data.frame(out.data, output)
-  if(!is.null(plot)) {
-    dev.copy(png, plot, width=600, height=600)
-    dev.off()
-  }
+  write.plot(plot)
 }
 
 run<-function() {
-  trycatch({
-    con1<-file("output/run.log","w")
+  basepath<-getwd()
+  for(k in 1:10) {
+    message("Run #",k)
+    outpath<-sprintf("output/run%02d",k)
+    dir.create(outpath)
+    setwd(outpath)
+    
+    con1<-file("run.log","w")
     sink(con1, append=TRUE)
-
-    con2<-file("output/msg.log","w")
+    
+    con2<-file("msg.log","w")
     sink(con2, append=TRUE, type="message")
-  
+    
     for(name in names(scenario)) {
       message("=== BEGIN ",Sys.time()," ===",appendLF=TRUE)
       message("Scenario: ",name,appendLF=TRUE)
@@ -109,12 +110,10 @@ run<-function() {
       message(sprintf("   %-10s: %s\n",names(lst),lst))
       message("=== END ",Sys.time()," ===",appendLF=TRUE)
     }
-  },
-  error = function(err) { stop(err) },
-  warning = function(war) { warning(war) },
-  finally = {
-    sink() 
-    sink(type="message")
-  })
+    
+    sink()
+    sink("message")
+    
+    setwd(basepath)
+  }
 }
-
